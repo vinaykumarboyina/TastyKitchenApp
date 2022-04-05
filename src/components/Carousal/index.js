92a19f912914d237eda6757a2d059f0a604ad885
vinaykumar <vinaykumarboyina312@gmail.com>
2022-04-05 13:36:34 +0000

    Tasty Kitchen app

---
+++ src/components/Carousal/index.js
@@ -0,0 +1,77 @@
+import {Component} from 'react'
+import Cookies from 'js-cookie'
+import Slider from 'react-slick'
+import Loader from 'react-loader-spinner'
+import './index.css'
+
+class Carousal extends Component {
+  state = {
+    offersList: [],
+    isLoading: false,
+  }
+
+  componentDidMount() {
+    this.getCarousal()
+  }
+
+  getCarousal = async () => {
+    this.setState({isLoading: true})
+    const jwtToken = Cookies.get('jwt_token')
+    const url = 'https://apis.ccbp.in/restaurants-list/offers'
+    const options = {
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+      },
+      method: 'GET',
+    }
+    const response = await fetch(url, options)
+    const data = await response.json()
+    const updatedData = data.offers.map(eachItem => ({
+      id: eachItem.id,
+      imageUrl: eachItem.image_url,
+    }))
+    this.setState({offersList: updatedData, isLoading: false})
+  }
+
+  renderCarousal = () => {
+    const settings = {
+      dots: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+    }
+    const {offersList} = this.state
+    return (
+      <ul className="carousel-container">
+        <Slider {...settings} className="carousal">
+          {offersList.map(eachImage => (
+            <li key={eachImage.id}>
+              <img
+                src={eachImage.imageUrl}
+                alt="offer"
+                className="carousal-item-image"
+              />
+            </li>
+          ))}
+        </Slider>
+      </ul>
+    )
+  }
+
+  renderLoader = () => (
+    <div
+      testid="restaurants-offers-loader"
+      className="restaurants-offers-loader"
+    >
+      <Loader type="ThreeDots" color="#F7931E" height={50} width={50} />
+    </div>
+  )
+
+  render() {
+    const {isLoading} = this.state
+    return isLoading ? this.renderLoader() : this.renderCarousal()
+  }
+}
+
+export default Carousal
---
+++ src/components/Home/index.js
@@ -0,0 +1,28 @@
+import Cookies from 'js-cookie'
+import {Redirect} from 'react-router-dom'
+
+import Header from '../Header'
+
+import Carousal from '../Carousal'
+import PopularRestaurants from '../PopularRestaurants'
+import Footer from '../Footer'
+import './index.css'
+
+const Home = () => {
+  const jwtToken = Cookies.get('jwt_token')
+  if (jwtToken === undefined) {
+    return <Redirect to="/login" />
+  }
+
+  return (
+    <>
+      <Header />
+      <div className="home-container">
+        <Carousal />
+        <PopularRestaurants />
+      </div>
+      <Footer />
+    </>
+  )
+}
+export default Home
